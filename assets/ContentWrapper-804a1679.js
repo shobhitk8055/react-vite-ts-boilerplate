9028441df9c2e91bdfcaf2bb7ea1bc405f65fa22
shobhit mac <shobhitk8055@gmail.com>
2023-10-23 20:05:03 +0530

    Updates

---
+++ assets/ContentWrapper-804a1679.js
@@ -0,0 +1,1 @@
+import{j as a,A as i,w as s,H as n}from"./index-6537d5d6.js";const r=({title:e,children:t})=>a.jsx(i,{children:a.jsxs(s.div,{initial:{opacity:0,x:-10},animate:{opacity:1,x:0},exit:{opacity:0,x:1e3},className:"p-4",children:[a.jsx(n,{title:e}),t]})});export{r as C};
